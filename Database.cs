d9724d82fe8046f9d477191955afc12879a4d573
KajtazA1 <kajtaza1@s2.rpn.ch>
2021-09-18 13:08:25 +0200

    initial commit

---
+++ Database.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+namespace DatabaseCS
+{
+
+    public static class Database
+    {
+        private static string DB_PATH;
+        private static MySqlConnection sql_con;
+        private static MySqlDataReader rdr;
+        public static bool IsConnected
+        {
+            get
+            {
+                bool toreturn = false;
+                if (sql_con != null)
+                {
+                    toreturn = true;
+                }
+                return toreturn;
+            }
+        }
+        public static void Connect(string Host, string DatabaseName, string User, string Password)
+        {
+            try
+            {
+
+                Init(Host, DatabaseName, User, Password);
+                sql_con = new MySqlConnection(DB_PATH);
+                sql_con.Open();
+                rdr = null;
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+
+        }
+        public static bool Connect(string Host, string DatabaseName, string User)
+        {
+            try
+            {
+                Init(Host, DatabaseName, User, "");
+                sql_con = new MySqlConnection(DB_PATH);
+                sql_con.Open();
+                rdr = null;
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            return true;
+        }
+
+        private static void Init(string Host, string DatabaseName, string User, string Password)
+        {
+            DB_PATH = $"server = {Host}; user={User};password={Password};database={DatabaseName};SslMode=none";
+        }
+
+        /// <summary>
+        /// Function that allows you to execute a SELECT sql request
+        ///  Return a list of dictionary containing the datas returned by your sql server
+        /// </summary>
+        /// <param name="strRequete">Sql request</param>
+        /// <returns>Tableau 2 dimensions contenant le resultat du SELECT</returns>
+        /// <example>
+        /// <code>
+        /// string[,] tabAllUsers = db.Select("SELECT prenom, nom FROM tbl_users");
+        /// </code>
+        /// </example>
+        public static List<Dictionary<string, string>> Select(this string strRequete)
+        {
+
+            if (!IsConnected)
+            {
+                throw new Exception("Cannot execute a request if the database is not connected ! ");
+            }
+            // recuperation de la requete passé en paramètre et éxecutions 
+            var cmd = new MySqlCommand(strRequete, sql_con);
+            // execute la requete
+            List<Dictionary<string, string>> lstdata = new List<Dictionary<string, string>>();
+            cmd = new MySqlCommand(strRequete, sql_con);
+            rdr = cmd.ExecuteReader();
+            while (rdr.Read())
+            {
+                // Switch permettant de recuperer la valeur en fonction de leur type
+                Dictionary<string, string> data = new Dictionary<string, string>();
+                for (int i = 0; i < rdr.FieldCount; i++)
+                {
+                    string strDataType = rdr.GetDataTypeName(i).Split('(')[0];
+                    switch (strDataType)
+                    {
+                        case "TINYINT":
+                        case "INT":
+                        case "int":
+                        case "INTEGER":
+                        case "integer":
+                            //tab[i, iCpt] = Convert.ToString(rdr.GetInt32(i));
+                            data.Add(rdr.GetName(i), Convert.ToString(rdr.GetInt32(i)));
+                            break;
+                        case "VARCHAR":
+                            data.Add(rdr.GetName(i), Convert.ToString(rdr.GetString(i)));
+                            break;
+                        case "DATETIME":
+                            data.Add(rdr.GetName(i), Convert.ToString(rdr.GetDateTime(i)));
+                            break;
+                        case "main":
+                            data.Add(rdr.GetName(i), Convert.ToString(rdr.GetValue(i)));
+                            break;
+                        case "BOOL":
+                        case "BOOLEAN":
+                            data.Add(rdr.GetName(i), Convert.ToString(rdr.GetBoolean(i)));
+                            break;
+                        default:
+                            data.Add(rdr.GetName(i), Convert.ToString(rdr.GetValue(i)));
+                            break;
+                    }
+                }
+                lstdata.Add(data);
+            }
+            rdr.Close();
+            return lstdata;
+        }
+        /// <summary>
+        /// Execute une requête SQL de type écritures, example, INSERT INTO, UPDATE etc...
+        /// </summary>
+        /// <param name="strRequete"></param>
+        public static void Write(this string strRequete)
+        {
+            if (!IsConnected)
+            {
+                throw new Exception("Cannot execute a request if the database is not connected ! ");
+            }
+            var cmd = new MySqlCommand(strRequete, sql_con);
+            cmd.ExecuteNonQuery();
+
+        }
+        public static void Close()
+        {
+            if (IsConnected)
+                sql_con.Close();
+        }
+
+    }
+
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,94 @@
-# DatabaseCS
-This is the source code of my DatabaseCS library, feel free to use it, upgrade it, do anything you want with it.
+
+# Database.cs
+#### Small note
+In the examples right below you'll see that i use a lot of strings variables starting with `$"my string"` it is because it allow me to easily concat variables into my string, it also automatically convert the type of the variable into a string.
+
+Also please note that english is not my main language, i'm a computer science student and as i use this library a lot i've choosed to share it with you :3
+## Depedencies
+This packages is using `mysql.data`
+## How to use it
+After downloading the project you just have to include it in your usings using -> `using DatabaseCS`.
+
+After that you'll have to establish the connection with the database using the  `Database.Connect()` (do not forget to surround it with a **try-catch** because an error will be thrown if the connection failed)
+The connection method has an overloard containing a password in case if your database needs a password.
+Argument -> `(      (Server, Database, Username, (Password)     )`
+##### Example without password
+```cs
+//Establish a connection without a password
+Database.Connect("localhost", "MyDb", "root");
+``` 
+##### Exeample with a password
+```cs
+//Establish a connection with a password
+Database.Connect("localhost", "MyDb", "root", "password");
+``` 
+### Utilisation
+Since the 1.0.2 the usage of this library is way easier, and also faster.
+<sub>**\*Before attempting to execute ANY request you HAVE to establish a connection with the server.**</sub>
+- Select
+The Select method return a list of dictionnary containing your data, the keys to the date is the column name. 
+It might look a bit scary but using a **var** type variable makes it easier. 
+
+There are two ways of executing a select request.
+##### Exemple standard
+
+```cs
+var Data = Database.Select($"SELECT prenom, nom, age FROM tbl_users WHERE id={this.Id}");
+```
+<sub>(The argument of the method is a String, you can also put a string variable containing your request in here)</sub>
+##### Example using the extension method
+
+```cs
+var Data = strRequest.Select();
+```
+or
+```cs
+var Data = "SELECT * FROM tbl_users".Select();
+```
+_________________
+
+- How to use the fetched datas
+Once the select done you can check all of the datas using a foreach.
+
+
+##### If you know that your request only return one record you can do something like this
+```cs
+var Data = strRequete.Select();
+Console.WriteLine(Data[0]["firstname"]);
+```
+<sub>This will display in the console the firstname of the first fetched record.</sub>
+- This example will display the names of all of the user in my db, user by user
+
+```cs
+var Data = "SELECT * FROM tbl_users".Select();
+foreach(Dictionnary<string, string> row in Data){
+Console.WriteLine(row["name"]);
+}
+```
+
+If this example looks a bit too complicated you can also do it like this : 
+```cs
+var Data = "SELECT * FROM tbl_users".Select();
+foreach(var row in Data){
+Console.WriteLine(row[nom]);
+}
+```
+<sub> **Be careful ! doing it this way might frighten your teachers..**</sub>
+- Insert
+<sub>The argument to pass in parameter is your sql request.</sub>
+
+```cs
+Database.Write($"INSERT INTO tbl_users(prenom, nom, age) VALUES(\"{firstname}\",\"{name}\",{age}");
+```
+Or
+```cs
+$"INSERT INTO tbl_users(prenom, nom, age) VALUES(\"{firstname}\",\"{name}\",{age}".Write();
+```
+- Update
+```cs
+Database.Write($"UPDATE tbl_users SET age={age} WHERE nom = \"{name}\"");
+```
+OU
+```cs
+$"UPDATE tbl_users SET age={age} WHERE nom = \"{name}\"".Write();
+```
